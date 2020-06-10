450bdb759014eb95221aaaea62dc77a4a3805bc8
Crowdbotics <team@crowdbotics.com>
2020-06-10 13:13:28 +0000

    Update models

--- backend/pkmob_17987/wsgi.py
+++ backend/pkmob_17987/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pkmob_17987.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pkmob_17987.settings")
 
 application = get_wsgi_application()
